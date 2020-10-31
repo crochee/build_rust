03adfae15e90e30af05bc07f5a39e447ed3e52a5
OnlyOneFace <13522570308@139.com>
2020-11-01 02:30:09 +0800

    回退err

--- src/controller/build.rs
+++ src/controller/build.rs
@@ -1,15 +1,14 @@
 use crate::err::ResponseError;
-use actix_web::{post, web, HttpResponse};
+use actix_web::{post, web, Responder};
 use serde::Deserialize;
 
 #[post("/build/plugin/{name}")]
 pub async fn build_plugin(
     name: web::Path<String>,
     plugin_content: web::Json<PluginContent>,
-) -> HttpResponse {
+) -> impl Responder {
     println!("{},{:#?}", name, plugin_content);
-    let err = ResponseError::new(500, String::from("recovery"));
-    HttpResponse::from_error(err)
+    web::Json(ResponseError::new(500, String::from("recovery")))
 }
 
 #[derive(Debug, Deserialize)]
