d38f480f86a8a76cd3205f8acc6f96b84b15dcc7
OnlyOneFace <13522570308@139.com>
2020-11-01 02:01:25 +0800

    结构体声明变为公有

--- src/controller/build.rs
+++ src/controller/build.rs
@@ -1,18 +1,19 @@
-use actix_web::{post, web, Responder};
-use serde::Serialize;
 use crate::err::ResponseError;
+use actix_web::{post, web, Responder};
+use serde::Deserialize;
 
 #[post("/build/plugin/{name}")]
 pub async fn build_plugin(
     name: web::Path<String>,
     plugin_content: web::Json<PluginContent>,
 ) -> impl Responder {
+    println!("{},{:#?}", name, plugin_content);
     web::Json(ResponseError::new(500, String::from("recovery")))
 }
 
-#[derive(Serialize)]
-struct PluginContent {
-    id:usize,
-    path:String,
-    content:String,
+#[derive(Debug, Deserialize)]
+pub struct PluginContent {
+    pub id: usize,
+    pub path: String,
+    pub content: String,
 }
